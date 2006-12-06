44b742097c26d3d48bf1a25517b7e01be59e3458
niclas@hedhman.org <niclas@hedhman.org@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2006-12-06 04:32:12 +0000

    Making sure Pierre's module compiles.

--- log4jclient/src/main/java/org/ops4j/pax/log4jclient/pub/activator/Log4jClientActivator.java
+++ log4jclient/src/main/java/org/ops4j/pax/log4jclient/pub/activator/Log4jClientActivator.java
@@ -1,56 +1,52 @@
+/*
+ *   Copyright 2006 Pierre Parrand
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
 package org.ops4j.pax.log4jclient.pub.activator;
 
-import java.util.Properties;
-
+import org.apache.log4j.Logger;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.framework.InvalidSyntaxException;
 
-import org.apache.log4j.Logger;
+public class Log4jClientActivator
+    implements BundleActivator
+{
 
-public class Log4jClientActivator implements BundleActivator{
-
-    public void start(BundleContext context)
+    public void start( BundleContext context )
     {
-	System.out.println("Bundle log4jClient started");
+        System.out.println( "Bundle log4jClient started" );
 
-	//	try{
-	    //Properties log4jprops = new Properties();
-	    //log4jprops.put("logger.name", "testLogger");
-	    //String filter = "(logger.name=demoLogger)";
-	    //String filterShort = "loggerName";	    
-	    //ServiceReference[] srs = context.getServiceReferences(Logger.class.getName(), filter);
-	    //if(srs.length>0){
-	    String demoLog = "demoLogger";
-	    Logger logger = Logger.getLogger(demoLog);
-	    logger.warn("this is a warning");
-	    logger.error("this is an error");
+        String demoLog = "demoLogger";
+        Logger logger = Logger.getLogger( demoLog );
+        logger.warn( "this is a warning" );
+        logger.error( "this is an error" );
 
-	    String develLog = "develLogger";
-	    Logger logger2 = Logger.getLogger(develLog);
-	    logger2.warn("this is a warning");
-	    logger2.error("this is an error");
+        String develLog = "develLogger";
+        Logger logger2 = Logger.getLogger( develLog );
+        logger2.warn( "this is a warning" );
+        logger2.error( "this is an error" );
 
-	    String anonymousLog = "anonymousLogger";
-	    Logger logger3 = Logger.getLogger(anonymousLog);
-	    logger3.warn("this is a warning");
-	    logger3.error("this is an error");
-	    //logger.log(Level.WARN, "this is another warning");
-	    //}
-	    //else{
-	    //System.out.println("no service found");
-	    //}
-	    /*}
-	catch(InvalidSyntaxException e){
-	    e.printStackTrace();
-	}
-	    */
+        String anonymousLog = "anonymousLogger";
+        Logger logger3 = Logger.getLogger( anonymousLog );
+        logger3.warn( "this is a warning" );
+        logger3.error( "this is an error" );
     }
 
-    public void stop(BundleContext context)
+    public void stop( BundleContext context )
     {
-	System.out.println("Bundle log4jClient stopped");
+        System.out.println( "Bundle log4jClient stopped" );
     }
-    
+
 }
