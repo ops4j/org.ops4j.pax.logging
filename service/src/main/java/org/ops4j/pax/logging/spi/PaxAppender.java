a7f80989dbb7ec802eee8ff77f8c75e8917023bf
niclas@hedhman.org <niclas@hedhman.org@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-10-26 11:00:35 +0000

    File wasn't committed.

---
+++ service/src/main/java/org/ops4j/pax/logging/spi/PaxAppender.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2006 Niclas Hedhman.
+ *
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+package org.ops4j.pax.logging.spi;
+
+public interface PaxAppender
+{
+
+    /**
+     *  @return the name of this PaxAppender.
+     */
+    String getName();
+
+    /**
+     * Log in <code>Appender</code> specific way. When appropriate,
+     * Loggers will call the <code>doAppend</code> method of appender
+     * implementations in order to log.
+     *
+     * @param event The PaxLoggingEvent that has occurred.
+     */
+    void doAppend( PaxLoggingEvent event );
+
+
+}
