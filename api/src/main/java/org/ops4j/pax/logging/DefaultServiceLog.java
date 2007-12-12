6892ed144db60991ef29548351c094d8a8286a90
chirino <chirino@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-12-12 21:31:38 +0000

    Fix for issue: http://issues.ops4j.org/jira/browse/PAXLOGGING-16 Support setting the default log level that the DefaultServiceLog uses.
    Now controled via a System propertly.  It still defaults to DEBUG but to change it, you can
    add the following JVM argument to set it to INFO:
    -Dorg.ops4j.pax.logging.DefaultServiceLog.level=INFO

--- api/src/main/java/org/ops4j/pax/logging/DefaultServiceLog.java
+++ api/src/main/java/org/ops4j/pax/logging/DefaultServiceLog.java
@@ -1,6 +1,7 @@
 /*
  * Copyright 2001-2004 The Apache Software Foundation.
  * Copyright 2005 Niclas Hedhman
+ * Copyright 2007 Hiram Chirino
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +22,16 @@ import org.osgi.framework.Bundle;
 public class DefaultServiceLog
     implements PaxLogger
 {
+    private static final int TRACE = 0;
+    private static final int DEBUG = 1;
+    private static final int INFO  = 2;
+    private static final int WARN  = 3;
+    private static final int ERROR = 4;
+    private static final int FATAL = 5;
+    private static final int NONE  = 6;
 
+    public static int level = getDefaultLevel();
+    
     private Bundle m_bundle;
     private String m_categoryName;
 
@@ -31,89 +41,110 @@ public class DefaultServiceLog
         m_categoryName = categoryName;
     }
 
-    public boolean isTraceEnabled()
+    /**
+     * Gets the level that the logger will use.  Defaults to DEBUG but can be changed if
+     * if the "org.ops4j.pax.logging.DefaultServiceLog.level" system property is set to
+     * on of the following: TRACE, DEBUG, INFO, WARN, ERROR, FATAL, or NONE.
+     * 
+     * The System property must be set before the class is loaded.
+     * @return
+     */
+    static private int getDefaultLevel() {
+    	String levelName = System.getProperty("org.ops4j.pax.logging.DefaultServiceLog.level", "DEBUG").trim();
+    	if( "TRACE".equals(levelName) ) {
+    		return TRACE;
+    	} else if( "DEBUG".equals(levelName) ) {
+    		return DEBUG;
+    	} else if( "INFO".equals(levelName) ) {
+    		return INFO;
+    	} else if( "WARN".equals(levelName) ) {
+    		return WARN;
+    	} else if( "ERROR".equals(levelName) ) {
+    		return ERROR;
+    	} else if( "FATAL".equals(levelName) ) {
+    		return FATAL;
+    	} else if( "NONE".equals(levelName) ) {
+    		return NONE;
+    	} else { 
+    		return DEBUG;
+    	}
+	}
+
+	public boolean isTraceEnabled()
     {
-        return false;
+        return level <= TRACE;
     }
 
     public boolean isDebugEnabled()
     {
-        return true;
+        return level <= DEBUG;
     }
 
     public boolean isWarnEnabled()
     {
-        return true;
+        return level <= WARN;
     }
 
     public boolean isInfoEnabled()
     {
-        return true;
+        return level <= INFO;
     }
 
     public boolean isErrorEnabled()
     {
-        return true;
+        return level <= ERROR;
     }
 
     public boolean isFatalEnabled()
     {
-        return true;
+        return level <= FATAL;
     }
 
     public void trace( String message, Throwable t )
     {
+    	if( isTraceEnabled() ) {
+            output( message, t );
+    	}
     }
 
     public void debug( String message, Throwable t )
     {
-        output( message );
-        if( t != null )
-        {
-            t.printStackTrace( System.out );
-        }
+    	if( isDebugEnabled() ) {
+            output( message, t );
+    	}
     }
 
     public void inform( String message, Throwable t )
     {
-        output( message );
-        if( t != null )
-        {
-            t.printStackTrace( System.out );
-        }
+    	if( isInfoEnabled() ) {
+            output( message, t );
+    	}
     }
 
     public void warn( String message, Throwable t )
     {
-        output( message );
-        if( t != null )
-        {
-            t.printStackTrace( System.out );
-        }
+    	if( isWarnEnabled() ) {
+            output( message, t );
+    	}
     }
 
     public void error( String message, Throwable t )
     {
-        output( message );
-        if( t != null )
-        {
-            t.printStackTrace( System.out );
-        }
+    	if( isErrorEnabled() ) {
+            output( message, t );
+    	}
     }
 
     public void fatal( String message, Throwable t )
     {
-        output( message );
-        if( t != null )
-        {
-            t.printStackTrace( System.out );
-        }
+    	if( isFatalEnabled() ) {
+            output( message, t );
+    	}
     }
 
     public int getLogLevel()
     {
-        // Always DEBUG mode.
-        return 10000;
+        return level;
     }
 
     public String getName()
@@ -121,16 +152,21 @@ public class DefaultServiceLog
         return m_categoryName;
     }
 
-    private void output( String message )
+    private void output( String message, Throwable t )
     {
         // Might be [null] if used by standard test cases.
         if( m_bundle != null )
         {
             System.out.print( m_bundle.getSymbolicName() );
         }
+        
         System.out.print( "[" );
         System.out.print( m_categoryName );
         System.out.print( "] : " );
         System.out.println( message );
+        
+        if( t !=null ) {
+        	t.printStackTrace( System.out );
+        }
     }
 }
