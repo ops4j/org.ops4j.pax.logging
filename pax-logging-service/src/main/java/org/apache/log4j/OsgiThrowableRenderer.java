0aff56fd9ea507543bef0cbf34da126721d17e22
gnodet <gnodet@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2010-04-26 12:14:21 +0000

    PAXLOGGING-80: Add an OSGi enhanced throwable rendered to indicate bundle information in the stack trace

---
+++ pax-logging-service/src/main/java/org/apache/log4j/OsgiThrowableRenderer.java
@@ -0,0 +1,205 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.log4j;
+
+import org.apache.log4j.spi.ThrowableRenderer;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.security.CodeSource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Enhanced implementation of ThrowableRenderer.
+ * It displays bundle information if available next to the class name in the stack trace.
+ */
+public final class OsgiThrowableRenderer implements ThrowableRenderer {
+
+    private SecurityManagerEx sm = new SecurityManagerEx();
+
+
+    /**
+     * Construct new instance.
+     */
+    public OsgiThrowableRenderer() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String[] doRender(final Throwable throwable) {
+        try {
+            StackTraceElement[] elements = throwable.getStackTrace();
+            String[] lines = new String[elements.length + 1];
+            lines[0] = throwable.toString();
+            Map classMap = new HashMap();
+            Class[] classCtx;
+            try {
+                classCtx = (Class[]) Exception.class.getMethod("getClassContext", null).invoke(throwable, null);
+            } catch (Exception e) {
+                classCtx = sm.getClassContext();
+            }
+            Class lastClass = null;
+            for (int i = 0; i < elements.length && i < classCtx.length; i++) {
+                Class clazz = classCtx[classCtx.length - 1 - i];
+                if (elements[elements.length - 1 - i].getClassName().equals(clazz.getName())) {
+                    String classDetails = getClassDetail(clazz);
+                    classMap.put(clazz.getName(), classDetails);
+                    lastClass = clazz;
+                } else if (lastClass != null) {
+                    try {
+                        clazz = lastClass.getClassLoader().loadClass(elements[elements.length - 1 - i].getClassName());
+                        String classDetails = getClassDetail(clazz);
+                        classMap.put(clazz.getName(), classDetails);
+                        lastClass = clazz;
+                    } catch (ClassNotFoundException e) {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+            for(int i = 0; i < elements.length; i++) {
+                lines[i+1] = formatElement(elements[i], classMap);
+            }
+            return lines;
+        } catch(Exception ex) {
+        }
+        return DefaultThrowableRenderer.render(throwable);
+    }
+
+    /**
+     * Format one element from stack trace.
+     * @param element element, may not be null.
+     * @param classMap map of class name to location.
+     * @return string representation of element.
+     */
+    private String formatElement(final StackTraceElement element, final Map classMap) {
+        StringBuffer buf = new StringBuffer("\tat ");
+        buf.append(element);
+        try {
+            String className = element.getClassName();
+            Object classDetails = classMap.get(className);
+            if (classDetails == null) {
+                Class cls = findClass(className);
+                classDetails = getClassDetail(cls);
+                classMap.put(className, classDetails);
+            }
+            if (classDetails != null) {
+                buf.append(classDetails);
+            }
+        } catch(Exception ex) {
+        }
+        return buf.toString();
+    }
+
+    private String getClassDetail(Class cls) {
+        try {
+            Method getBundleMethod = FrameworkUtil.class.getMethod("getBundle", new Class[] { Class.class });
+            Bundle bundle = (Bundle) getBundleMethod.invoke(null, new Object[] { cls });
+            StringBuffer buf = new StringBuffer();
+            buf.append('[');
+            buf.append(bundle.getBundleId());
+            buf.append(":");
+            buf.append(bundle.getSymbolicName());
+            buf.append(":");
+            buf.append(bundle.getHeaders().get(org.osgi.framework.Constants.BUNDLE_VERSION));
+            buf.append(']');
+            return buf.toString();
+        } catch (Exception e) {
+        }
+
+        StringBuffer buf = new StringBuffer();
+        buf.append('[');
+        try {
+            CodeSource source = cls.getProtectionDomain().getCodeSource();
+            if (source != null) {
+                URL locationURL = source.getLocation();
+                if (locationURL != null) {
+                    //
+                    //   if a file: URL
+                    //
+                    if ("file".equals(locationURL.getProtocol())) {
+                        String path = locationURL.getPath();
+                        if (path != null) {
+                            //
+                            //  find the last file separator character
+                            //
+                            int lastSlash = path.lastIndexOf('/');
+                            int lastBack = path.lastIndexOf(File.separatorChar);
+                            if (lastBack > lastSlash) {
+                                lastSlash = lastBack;
+                            }
+                            //
+                            //  if no separator or ends with separator (a directory)
+                            //     then output the URL, otherwise just the file name.
+                            //
+                            if (lastSlash <= 0 || lastSlash == path.length() - 1) {
+                                buf.append(locationURL);
+                            } else {
+                                buf.append(path.substring(lastSlash + 1));
+                            }
+                        }
+                    } else {
+                        buf.append(locationURL);
+                    }
+                }
+            }
+        } catch(SecurityException ex) {
+        }
+        buf.append(':');
+        Package pkg = cls.getPackage();
+        if (pkg != null) {
+            String implVersion = pkg.getImplementationVersion();
+            if (implVersion != null) {
+                buf.append(implVersion);
+            }
+        }
+        buf.append(']');
+        return buf.toString();
+    }
+
+    /**
+     * Find class given class name.
+     * @param className class name, may not be null.
+     * @return class, will not be null.
+     * @throws ClassNotFoundException thrown if class can not be found.
+     */
+    private Class findClass(final String className) throws ClassNotFoundException {
+        try {
+            return Thread.currentThread().getContextClassLoader().loadClass(className);
+        } catch (ClassNotFoundException e) {
+            try {
+                return Class.forName(className);
+            } catch (ClassNotFoundException e1) {
+                return getClass().getClassLoader().loadClass(className);
+            }
+        }
+    }
+
+    static class SecurityManagerEx extends SecurityManager
+    {
+        public Class[] getClassContext()
+        {
+            return super.getClassContext();
+        }
+    }
+}
