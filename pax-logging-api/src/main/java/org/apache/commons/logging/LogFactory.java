905f0bf20fb65ee83b2c35fd34360c2f29ddf0a9
Andreas Pieber <anpieber@gmail.com>
2011-06-29 11:02:47 +0200

    [PAXLOGGING-110] Fixed ConcurrentModificationException in LogFactory
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- pax-logging-api/src/main/java/org/apache/commons/logging/LogFactory.java
+++ pax-logging-api/src/main/java/org/apache/commons/logging/LogFactory.java
@@ -23,29 +23,26 @@
 
 package org.apache.commons.logging;
 
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.WeakHashMap;
+
 import org.apache.commons.logging.internal.JclLogger;
-import org.osgi.framework.BundleContext;
-import org.ops4j.pax.logging.internal.DefaultServiceLog;
-import org.ops4j.pax.logging.internal.FallbackLogFactory;
 import org.ops4j.pax.logging.OSGIPaxLoggingManager;
 import org.ops4j.pax.logging.PaxLogger;
 import org.ops4j.pax.logging.PaxLoggingManager;
+import org.ops4j.pax.logging.internal.FallbackLogFactory;
+import org.osgi.framework.BundleContext;
 
 /**
  * This is an adaptation of the Jakarta Commons Logging API for OSGi usage.
  * <p>
- * The client code that wishes to use this adaptation of Jakarta Commons Logging
- * and have the log output to be directed to the Pax Logging Service backend,
- * which is driven by Log4J, it is necessary to;
+ * The client code that wishes to use this adaptation of Jakarta Commons Logging and have the log output to be directed
+ * to the Pax Logging Service backend, which is driven by Log4J, it is necessary to;
  * <ul>
- * <li>ensure that commons-logging.jar is <b>NOT</b> included in your bundle
- * jar.</li>
+ * <li>ensure that commons-logging.jar is <b>NOT</b> included in your bundle jar.</li>
  * <li>include the pax-logging-client.jar into the client bundle.</li>
  * <li>update your Manifest.MF to import the org.ops4j.pax.logging package.</li>
  * <li>Initiate this class by setting the Bundle Context.</li>
@@ -53,7 +50,7 @@ import org.ops4j.pax.logging.PaxLoggingManager;
  * Typical code looks like this; <code><pre>
  * import org.apache.commons.logging.LogFactory;
  * import org.apache.commons.logging.Log;
- *
+ * 
  * public class Activator
  *         implements BundleActivator
  * {
@@ -64,7 +61,7 @@ import org.ops4j.pax.logging.PaxLoggingManager;
  * }
  * </pre></code>
  * </p>
- *
+ * 
  * @author Niclas Hedhman (responsible for the OSGi adaptation.)
  * @author Craig R. McClanahan
  * @author Costin Manolache
@@ -75,14 +72,13 @@ public class LogFactory
 {
 
     /**
-     * ps4j/pax/logging/providers The name of the property used to identify the
-     * LogFactory implementation class name.
+     * ps4j/pax/logging/providers The name of the property used to identify the LogFactory implementation class name.
      */
     public static final String FACTORY_PROPERTY = "org.apache.commons.logging.LogFactory";
 
     /**
-     * The fully qualified class name of the fallback <code>LogFactory</code>
-     * implementation class to use, if no other can be found.
+     * The fully qualified class name of the fallback <code>LogFactory</code> implementation class to use, if no other
+     * can be found.
      */
     public static final String FACTORY_DEFAULT = "org.apache.commons.logging.LogFactory";
 
@@ -100,30 +96,30 @@ public class LogFactory
     static
     {
         m_instance = new LogFactory();
-        m_loggers = Collections.synchronizedMap( new WeakHashMap() );
+        m_loggers = new WeakHashMap();
     }
 
-    public static void setBundleContext( BundleContext ctx )
+    public static void setBundleContext(BundleContext ctx)
     {
-        m_paxLogging = new OSGIPaxLoggingManager( ctx );
-
-        Set entrySet = m_loggers.entrySet();
-        Iterator iterator = entrySet.iterator();
-        while( iterator.hasNext() )
-        {
-            Map.Entry entry = (Entry) iterator.next();
-            JclLogger logger = (JclLogger) entry.getKey();
-            String name = (String) entry.getValue();
-            logger.setPaxLoggingManager( m_paxLogging, name );
+        synchronized (m_loggers) {
+            m_paxLogging = new OSGIPaxLoggingManager(ctx);
+            Set entrySet = m_loggers.entrySet();
+            Iterator iterator = entrySet.iterator();
+            while (iterator.hasNext())
+            {
+                Map.Entry entry = (Entry) iterator.next();
+                JclLogger logger = (JclLogger) entry.getKey();
+                String name = (String) entry.getValue();
+                logger.setPaxLoggingManager(m_paxLogging, name);
+            }
+            m_paxLogging.open();
         }
-        m_paxLogging.open();
     }
 
     /**
      * @return the LogFactory instance to use.
-     *
-     * @throws LogConfigurationException if the implementation class is not available or cannot be
-     *                                   instantiated.
+     * 
+     * @throws LogConfigurationException if the implementation class is not available or cannot be instantiated.
      */
     public static LogFactory getFactory()
         throws LogConfigurationException
@@ -132,58 +128,51 @@ public class LogFactory
     }
 
     /**
-     * Convenience method to return a named logger, without the application
-     * having to care about factories.
-     *
+     * Convenience method to return a named logger, without the application having to care about factories.
+     * 
      * @param clazz Class from which a log name will be derived
-     *
+     * 
      * @return the Log instance to use for the class
-     *
+     * 
      * @throws LogConfigurationException if a suitable <code>Log</code> instance cannot be returned
      */
-    public static Log getLog( Class clazz )
+    public static Log getLog(Class clazz)
         throws LogConfigurationException
     {
-        return getLog( clazz.getName() );
+        return getLog(clazz.getName());
     }
 
     /**
-     * Convenience method to return a named logger, without the application
-     * having to care about factories.
-     *
-     * @param name Logical name of the <code>Log</code> instance to be returned
-     *             (the meaning of this name is only known to the underlying
-     *             logging implementation that is being wrapped)
-     *
+     * Convenience method to return a named logger, without the application having to care about factories.
+     * 
+     * @param name Logical name of the <code>Log</code> instance to be returned (the meaning of this name is only known
+     *        to the underlying logging implementation that is being wrapped)
+     * 
      * @return the Log instance to use for the class of the given name
-     *
+     * 
      * @throws LogConfigurationException if a suitable <code>Log</code> instance cannot be returned
      */
-    public static Log getLog( String name )
+    public static Log getLog(String name)
         throws LogConfigurationException
     {
-        return getFactory().getInstance( name );
+        return getFactory().getInstance(name);
     }
 
     /**
-     * Release any internal references to previously created {@link LogFactory}
-     * instances that have been associated with the specified class loader (if
-     * any), after calling the instance method <code>release()</code> on each
-     * of them.
-     *
+     * Release any internal references to previously created {@link LogFactory} instances that have been associated with
+     * the specified class loader (if any), after calling the instance method <code>release()</code> on each of them.
+     * 
      * @param classLoader ClassLoader for which to release the LogFactory
      */
-    public static void release( ClassLoader classLoader )
+    public static void release(ClassLoader classLoader)
     {
     }
 
     /**
-     * Release any internal references to previously created {@link LogFactory}
-     * instances, after calling the instance method <code>release()</code> on
-     * each of them. This is useful in environments like servlet containers,
-     * which implement application reloading by throwing away a ClassLoader.
-     * Dangling references to objects in that class loader would prevent garbage
-     * collection.
+     * Release any internal references to previously created {@link LogFactory} instances, after calling the instance
+     * method <code>release()</code> on each of them. This is useful in environments like servlet containers, which
+     * implement application reloading by throwing away a ClassLoader. Dangling references to objects in that class
+     * loader would prevent garbage collection.
      */
     public static void releaseAll()
     {
@@ -194,23 +183,22 @@ public class LogFactory
     }
 
     /**
-     * Return the configuration attribute with the specified name (if any), or
-     * <code>null</code> if there is no such attribute.
-     *
+     * Return the configuration attribute with the specified name (if any), or <code>null</code> if there is no such
+     * attribute.
+     * 
      * @param name Name of the attribute to return
-     *
+     * 
      * @return always return null. This method is not supported in Pax Logging.
      */
-    public Object getAttribute( String name )
+    public Object getAttribute(String name)
     {
         return null;
     }
 
     /**
-     * Return an array containing the names of all currently defined
-     * configuration attributes. If there are no such attributes, a zero length
-     * array is returned.
-     *
+     * Return an array containing the names of all currently defined configuration attributes. If there are no such
+     * attributes, a zero length array is returned.
+     * 
      * @return always returns an emtpy String array. This method is not supported in Pax Logging.
      */
     public String[] getAttributeNames()
@@ -219,91 +207,85 @@ public class LogFactory
     }
 
     /**
-     * Convenience method to derive a name from the specified class and call
-     * <code>getInstance(String)</code> with it.
-     *
+     * Convenience method to derive a name from the specified class and call <code>getInstance(String)</code> with it.
+     * 
      * @param clazz Class for which a suitable Log name will be derived
-     *
+     * 
      * @return the Log instance to use for the given class.
-     *
+     * 
      * @throws LogConfigurationException if a suitable <code>Log</code> instance cannot be returned
      */
-    public Log getInstance( Class clazz )
+    public Log getInstance(Class clazz)
         throws LogConfigurationException
     {
-        return getInstance( clazz.getName() );
+        return getInstance(clazz.getName());
     }
 
     /**
      * <p>
-     * Construct (if necessary) and return a <code>Log</code> instance, using
-     * the factory's current set of configuration attributes.
+     * Construct (if necessary) and return a <code>Log</code> instance, using the factory's current set of configuration
+     * attributes.
      * </p>
-     *
+     * 
      * <p>
-     * <strong>NOTE</strong> - Depending upon the implementation of the
-     * <code>LogFactory</code> you are using, the <code>Log</code> instance
-     * you are returned may or may not be local to the current application, and
-     * may or may not be returned again on a subsequent call with the same name
-     * argument.
+     * <strong>NOTE</strong> - Depending upon the implementation of the <code>LogFactory</code> you are using, the
+     * <code>Log</code> instance you are returned may or may not be local to the current application, and may or may not
+     * be returned again on a subsequent call with the same name argument.
      * </p>
-     *
-     * @param name Logical name of the <code>Log</code> instance to be returned
-     *             (the meaning of this name is only known to the underlying
-     *             logging implementation that is being wrapped)
-     *
+     * 
+     * @param name Logical name of the <code>Log</code> instance to be returned (the meaning of this name is only known
+     *        to the underlying logging implementation that is being wrapped)
+     * 
      * @return the Log instance of the class with the given name.
-     *
+     * 
      * @throws LogConfigurationException if a suitable <code>Log</code> instance cannot be returned
      */
-    public Log getInstance( String name )
+    public Log getInstance(String name)
         throws LogConfigurationException
     {
         PaxLogger logger;
-        if( m_paxLogging == null )
+        if (m_paxLogging == null)
         {
-            logger = FallbackLogFactory.createFallbackLog( null, name );
+            logger = FallbackLogFactory.createFallbackLog(null, name);
         }
         else
         {
-            logger = m_paxLogging.getLogger( name, JclLogger.JCL_FQCN );
+            logger = m_paxLogging.getLogger(name, JclLogger.JCL_FQCN);
         }
-        JclLogger jclLogger = new JclLogger( logger );
-        m_loggers.put( jclLogger, name );
+        JclLogger jclLogger = new JclLogger(logger);
+        synchronized (m_loggers) {
+            m_loggers.put(jclLogger, name);
+        }
         return jclLogger;
     }
 
     /**
-     * Release any internal references to previously created {@link Log}
-     * instances returned by this factory. This is useful in environments like
-     * servlet containers, which implement application reloading by throwing
-     * away a ClassLoader. Dangling references to objects in that class loader
-     * would prevent garbage collection.
+     * Release any internal references to previously created {@link Log} instances returned by this factory. This is
+     * useful in environments like servlet containers, which implement application reloading by throwing away a
+     * ClassLoader. Dangling references to objects in that class loader would prevent garbage collection.
      */
     static public void release()
     {
     }
 
     /**
-     * Remove any configuration attribute associated with the specified name. If
-     * there is no such attribute, no action is taken.
-     *
+     * Remove any configuration attribute associated with the specified name. If there is no such attribute, no action
+     * is taken.
+     * 
      * @param name Name of the attribute to remove
      */
-    public void removeAttribute( String name )
+    public void removeAttribute(String name)
     {
     }
 
     /**
-     * Set the configuration attribute with the specified name. Calling this
-     * with a <code>null</code> value is equivalent to calling
-     * <code>removeAttribute(name)</code>.
-     *
-     * @param name  Name of the attribute to set
-     * @param value Value of the attribute to set, or <code>null</code> to
-     *              remove any setting for this attribute
+     * Set the configuration attribute with the specified name. Calling this with a <code>null</code> value is
+     * equivalent to calling <code>removeAttribute(name)</code>.
+     * 
+     * @param name Name of the attribute to set
+     * @param value Value of the attribute to set, or <code>null</code> to remove any setting for this attribute
      */
-    public void setAttribute( String name, Object value )
+    public void setAttribute(String name, Object value)
     {
     }
 
