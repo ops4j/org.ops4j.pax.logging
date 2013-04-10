9320dd23b7c1a758cd469e8d5604609c0d5769da
Christoph Emmersberger <cemmersb@googlemail.com>
2013-04-10 10:52:09 +0200

    +/- reformat class to comply to coding rules

--- pax-logging-it/src/test/java/org/ops4j/pax/logging/it/appender/CustomAppender.java
+++ pax-logging-it/src/test/java/org/ops4j/pax/logging/it/appender/CustomAppender.java
@@ -1,19 +1,19 @@
 /*  Copyright 2008 Edward Yakop.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-* implied.
-*
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ops4j.pax.logging.it.appender;
 
 import java.util.LinkedList;
@@ -25,20 +25,17 @@ import org.ops4j.pax.logging.spi.PaxLoggingEvent;
  * @author edward.yakop@gmail.com
  */
 final class CustomAppender
-    implements PaxAppender
-{
+    implements PaxAppender {
 
-    private final List events;
+  private final List events;
 
-    public CustomAppender()
-    {
-        events = new LinkedList();
-    }
+  public CustomAppender() {
+    events = new LinkedList();
+  }
 
-    public final List getEvents()
-    {
-        return events;
-    }
+  public final List getEvents() {
+    return events;
+  }
 
   public void doAppend(PaxLoggingEvent event) {
     if (null != events) {
@@ -47,8 +44,7 @@ final class CustomAppender
     }
   }
 
-    public String toString()
-    {
-        return "Custom appender";
-    }
+  public String toString() {
+    return "Custom appender";
+  }
 }
