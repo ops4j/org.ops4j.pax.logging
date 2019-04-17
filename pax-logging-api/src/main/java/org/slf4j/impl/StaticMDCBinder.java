93686acbfb7e79bddd9db0111b7a26317a578998
Grzegorz Grzybek <gr.grzybek@gmail.com>
2019-04-17 16:39:08 +0200

    [build] Take all, unchanged sources from org.slf4j:slf4j-api:1.7.26

--- pax-logging-api/src/main/java/org/slf4j/impl/StaticMDCBinder.java
+++ pax-logging-api/src/main/java/org/slf4j/impl/StaticMDCBinder.java
@@ -1,70 +1,67 @@
-/*
+/**
  * Copyright (c) 2004-2011 QOS.ch
- *
  * All rights reserved.
  *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
+ * Permission is hereby granted, free  of charge, to any person obtaining
+ * a  copy  of this  software  and  associated  documentation files  (the
  * "Software"), to  deal in  the Software without  restriction, including
  * without limitation  the rights to  use, copy, modify,  merge, publish,
- * distribute, and/or sell copies of  the Software, and to permit persons
- * to whom  the Software is furnished  to do so, provided  that the above
- * copyright notice(s) and this permission notice appear in all copies of
- * the  Software and  that both  the above  copyright notice(s)  and this
- * permission notice appear in supporting documentation.
+ * distribute,  sublicense, and/or sell  copies of  the Software,  and to
+ * permit persons to whom the Software  is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The  above  copyright  notice  and  this permission  notice  shall  be
+ * included in all copies or substantial portions of the Software.
  *
  * THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
  * EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR  A PARTICULAR PURPOSE AND NONINFRINGEMENT
- * OF  THIRD PARTY  RIGHTS. IN  NO EVENT  SHALL THE  COPYRIGHT  HOLDER OR
- * HOLDERS  INCLUDED IN  THIS  NOTICE BE  LIABLE  FOR ANY  CLAIM, OR  ANY
- * SPECIAL INDIRECT  OR CONSEQUENTIAL DAMAGES, OR  ANY DAMAGES WHATSOEVER
- * RESULTING FROM LOSS  OF USE, DATA OR PROFITS, WHETHER  IN AN ACTION OF
- * CONTRACT, NEGLIGENCE  OR OTHER TORTIOUS  ACTION, ARISING OUT OF  OR IN
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- *
- * Except as  contained in  this notice, the  name of a  copyright holder
- * shall not be used in advertising or otherwise to promote the sale, use
- * or other dealings in this Software without prior written authorization
- * of the copyright holder.
+ * MERCHANTABILITY,    FITNESS    FOR    A   PARTICULAR    PURPOSE    AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE,  ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  */
-
 package org.slf4j.impl;
 
 import org.slf4j.spi.MDCAdapter;
-import org.ops4j.pax.logging.slf4j.Slf4jMDCAdapter;
 
 /**
- * This class is only a stub. Real implementations are found in
+ * This class is only a stub. Real implementations are found in 
  * each SLF4J binding project, e.g. slf4j-nop, slf4j-log4j12 etc.
  *
  * @author Ceki G&uuml;lc&uuml;
  */
 public class StaticMDCBinder {
 
+    /**
+     * The unique instance of this class.
+     */
+    public static final StaticMDCBinder SINGLETON = new StaticMDCBinder();
 
-  /**
-   * The unique instance of this class.
-   */
-  public static final StaticMDCBinder SINGLETON = new StaticMDCBinder();
+    private StaticMDCBinder() {
+        throw new UnsupportedOperationException("This code should never make it into the jar");
+    }
 
-  private static final String mdcAdapterClassStr = Slf4jMDCAdapter.class.getName();
+    /**
+     * Return the singleton of this class.
+     * 
+     * @return the StaticMDCBinder singleton
+     * @since 1.7.14
+     */
+    public static final StaticMDCBinder getSingleton() {
+        return SINGLETON;
+    }
 
-  private static final MDCAdapter mdcAdapter = new Slf4jMDCAdapter();
+    /**
+     * Currently this method always returns an instance of 
+     * {@link StaticMDCBinder}.
+     */
+    public MDCAdapter getMDCA() {
+        throw new UnsupportedOperationException("This code should never make it into the jar");
+    }
 
-  private StaticMDCBinder() {
-  }
-
-  public static StaticMDCBinder getSingleton() {
-    return SINGLETON;
-  }
-
-  public MDCAdapter getMDCA() {
-    return mdcAdapter;
-  }
-
-  public String  getMDCAdapterClassStr() {
-    return mdcAdapterClassStr;
-  }
+    public String getMDCAdapterClassStr() {
+        throw new UnsupportedOperationException("This code should never make it into the jar");
+    }
 }
