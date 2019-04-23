2b883ab0464c66ae1115404f9fc08f47d5853148
Grzegorz Grzybek <gr.grzybek@gmail.com>
2019-04-23 16:37:02 +0200

    [PAXLOGGING-252] Reorganize integration tests with newest pax-exam

---
+++ pax-logging-it/src/test/java/org/ops4j/pax/logging/it/AbstractControlledIntegrationTestBase.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.ops4j.pax.logging.it;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.ops4j.pax.exam.Option;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.ops4j.pax.exam.Constants.START_LEVEL_SYSTEM_BUNDLES;
+import static org.ops4j.pax.exam.Constants.START_LEVEL_TEST_BUNDLE;
+import static org.ops4j.pax.exam.CoreOptions.bootDelegationPackage;
+import static org.ops4j.pax.exam.CoreOptions.cleanCaches;
+import static org.ops4j.pax.exam.CoreOptions.frameworkProperty;
+import static org.ops4j.pax.exam.CoreOptions.frameworkStartLevel;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.linkBundle;
+import static org.ops4j.pax.exam.CoreOptions.systemTimeout;
+import static org.ops4j.pax.exam.CoreOptions.url;
+import static org.ops4j.pax.exam.CoreOptions.workingDirectory;
+
+public class AbstractControlledIntegrationTestBase {
+
+    public static Logger LOG = LoggerFactory.getLogger(AbstractControlledIntegrationTestBase.class);
+
+    @Rule
+    public TestName testName = new TestName();
+
+    @Before
+    public void beforeEach() {
+        LOG.info("========== Running {}.{}() ==========", getClass().getName(), testName.getMethodName());
+    }
+
+    @After
+    public void afterEach() {
+        LOG.info("========== Finished {}.{}() ==========", getClass().getName(), testName.getMethodName());
+    }
+
+    protected Option[] baseConfigure() {
+        Option[] options = new Option[] {
+                // basic options
+                bootDelegationPackage("sun.*"),
+                bootDelegationPackage("com.sun.*"),
+
+                frameworkStartLevel(START_LEVEL_TEST_BUNDLE),
+
+                workingDirectory("target/paxexam"),
+                cleanCaches(true),
+                systemTimeout(60 * 60 * 1000),
+
+                // set to "4" to see Felix wiring information
+                frameworkProperty("felix.log.level").value("1"),
+
+                // added implicitly by pax-exam, if pax.exam.system=test
+                // these resources are provided inside org.ops4j.pax.exam:pax-exam-link-mvn jar
+                // for example, "link:classpath:META-INF/links/org.ops4j.base.link" = "mvn:org.ops4j.base/ops4j-base/1.5.0"
+                url("link:classpath:META-INF/links/org.ops4j.base.link").startLevel(START_LEVEL_SYSTEM_BUNDLES),
+                url("link:classpath:META-INF/links/org.ops4j.pax.swissbox.core.link").startLevel(START_LEVEL_SYSTEM_BUNDLES),
+                url("link:classpath:META-INF/links/org.ops4j.pax.swissbox.extender.link").startLevel(START_LEVEL_SYSTEM_BUNDLES),
+                url("link:classpath:META-INF/links/org.ops4j.pax.swissbox.framework.link").startLevel(START_LEVEL_SYSTEM_BUNDLES),
+                url("link:classpath:META-INF/links/org.ops4j.pax.swissbox.lifecycle.link").startLevel(START_LEVEL_SYSTEM_BUNDLES),
+                url("link:classpath:META-INF/links/org.ops4j.pax.swissbox.tracker.link").startLevel(START_LEVEL_SYSTEM_BUNDLES),
+                url("link:classpath:META-INF/links/org.ops4j.pax.exam.link").startLevel(START_LEVEL_SYSTEM_BUNDLES),
+                url("link:classpath:META-INF/links/org.ops4j.pax.exam.inject.link").startLevel(START_LEVEL_SYSTEM_BUNDLES),
+                url("link:classpath:META-INF/links/org.ops4j.pax.extender.service.link").startLevel(START_LEVEL_SYSTEM_BUNDLES),
+
+                linkBundle("org.apache.servicemix.bundles.javax-inject").startLevel(START_LEVEL_SYSTEM_BUNDLES),
+
+                junitBundles(),
+        };
+        return options;
+    }
+
+}
---
+++ pax-logging-it/src/test/java/org/ops4j/pax/logging/it/CleanIntegrationTest.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.ops4j.pax.logging.it;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.OptionUtils.combine;
+
+/**
+ * Clean test without any pax-logging bundles. Just simplest showcase of what's needed to run
+ * manually controlled pax-exam test.
+ */
+@RunWith(PaxExam.class)
+public class CleanIntegrationTest extends AbstractControlledIntegrationTestBase {
+
+    public static Logger LOG = LoggerFactory.getLogger(CleanIntegrationTest.class);
+
+    @Inject
+    private BundleContext context;
+
+    @Configuration
+    public Option[] configure() {
+        return combine(
+                baseConfigure(),
+                // pax-exam-extender-service bundle (org.ops4j.pax.exam.raw.extender.intern.Parser) requires
+                // SLF4J API:
+                // [org.ops4j.pax.exam.extender.service [9](R 9.0)] osgi.wiring.package; (&(osgi.wiring.package=org.slf4j)(version>=1.4.0)(!(version>=2.0.0)))
+                mavenBundle("org.slf4j", "slf4j-api", System.getProperty("version.org.slf4j")),
+                // while SLF4J API requires implementation (could be a fragment)
+                mavenBundle("org.slf4j", "slf4j-nop", System.getProperty("version.org.slf4j")).start(false)
+        );
+    }
+
+    @Test
+    public void justRun() {
+        Set<Bundle> bundles = new TreeSet<>((b1, b2) -> (int) (b1.getBundleId() - b2.getBundleId()));
+        bundles.addAll(Arrays.asList(context.getBundles()));
+        for (Bundle b : bundles) {
+            String info = String.format("#%d: %s (%s)", b.getBundleId(), b.getSymbolicName(), b.getLocation());
+            // System.out.println, thanks to failsafe's <redirectTestOutputToFile> will land in
+            // pax-logging-it/target/failsafe-reports/org.ops4j.pax.logging.it.CleanIntegrationTest-output.txt
+            System.out.println(info);
+            // slf4j-nop logger won't do anything
+            LOG.info(info);
+        }
+    }
+
+}
