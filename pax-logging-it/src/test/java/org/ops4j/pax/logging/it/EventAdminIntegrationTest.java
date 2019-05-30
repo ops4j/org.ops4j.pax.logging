0ee9a68ab5662ce7ecc8632a38182a9289235849
Grzegorz Grzybek <gr.grzybek@gmail.com>
2019-05-30 09:33:53 +0200

    [PAXLOGGING-252] Final itests related to event admin and LogReader service

---
+++ pax-logging-it/src/test/java/org/ops4j/pax/logging/it/EventAdminIntegrationTest.java
@@ -0,0 +1,139 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.ops4j.pax.logging.it;
+
+import java.io.IOException;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.logging.it.support.Helpers;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.ops4j.pax.exam.OptionUtils.combine;
+
+@RunWith(PaxExam.class)
+public class EventAdminIntegrationTest extends AbstractControlledIntegrationTestBase {
+
+    @Configuration
+    public Option[] configure() throws IOException {
+        return combine(
+                combine(baseConfigure(), defaultLoggingConfig()),
+
+                paxLoggingApi(),
+                paxLoggingLog4J1().noStart(),
+                paxLoggingLogback().noStart(),
+                paxLoggingLog4J2().noStart(),
+                configAdmin(),
+                eventAdmin()
+        );
+    }
+
+    @Test
+    public void eventAdminNotificationFromAll3Backends() throws Exception {
+        Logger logger = LoggerFactory.getLogger(EventAdminIntegrationTest.class);
+
+        Bundle paxLoggingLog4J1 = Helpers.paxLoggingService(context);
+        Bundle paxLoggingLogback = Helpers.paxLoggingLogback(context);
+        Bundle paxLoggingLog4J2 = Helpers.paxLoggingLog4j2(context);
+
+        Bundle probe = FrameworkUtil.getBundle(this.getClass());
+
+        final List<Event> events = new LinkedList<>();
+        Dictionary<String, Object> props = new Hashtable<>();
+        props.put(EventConstants.EVENT_TOPIC, new String[] {
+                "org/osgi/service/log/LogEntry/LOG_ERROR",
+                "org/osgi/service/log/LogEntry/LOG_WARNING",
+                "org/osgi/service/log/LogEntry/LOG_INFO",
+                "org/osgi/service/log/LogEntry/LOG_DEBUG",
+                "org/osgi/service/log/LogEntry/LOG_OTHER"
+        });
+        context.registerService(EventHandler.class, events::add, props);
+
+        logger.info("when no backend available");
+
+        paxLoggingLog4J1.start();
+
+        logger.info("when log4j1 available (info)");
+        logger.error("when log4j1 available (error)", new Exception("exception 1"));
+
+        paxLoggingLog4J1.stop();
+        paxLoggingLogback.start();
+
+        logger.info("when logback available (info)");
+        logger.error("when logback available (error)", new Exception("exception 2"));
+
+        paxLoggingLogback.stop();
+        paxLoggingLog4J2.start();
+
+        logger.info("when log4j2 available (info)");
+        logger.error("when log4j2 available (error)", new Exception("exception 3"));
+
+        Event e12 = ev(events, "when log4j1 available (info)");
+        Event e13 = ev(events, "when log4j1 available (error)");
+        Event e22 = ev(events, "when logback available (info)");
+        Event e23 = ev(events, "when logback available (error)");
+        Event e32 = ev(events, "when log4j2 available (info)");
+        Event e33 = ev(events, "when log4j2 available (error)");
+
+        assertThat(e12.getProperty("bundle"), equalTo(probe));
+        assertThat(e13.getProperty("bundle"), equalTo(probe));
+        assertThat(e22.getProperty("bundle"), equalTo(probe));
+        assertThat(e23.getProperty("bundle"), equalTo(probe));
+        assertThat(e32.getProperty("bundle"), equalTo(probe));
+        assertThat(e33.getProperty("bundle"), equalTo(probe));
+
+        assertThat(e12.getTopic(), equalTo("org/osgi/service/log/LogEntry/LOG_INFO"));
+        assertThat(e13.getTopic(), equalTo("org/osgi/service/log/LogEntry/LOG_ERROR"));
+        assertThat(e22.getTopic(), equalTo("org/osgi/service/log/LogEntry/LOG_INFO"));
+        assertThat(e23.getTopic(), equalTo("org/osgi/service/log/LogEntry/LOG_ERROR"));
+        assertThat(e32.getTopic(), equalTo("org/osgi/service/log/LogEntry/LOG_INFO"));
+        assertThat(e33.getTopic(), equalTo("org/osgi/service/log/LogEntry/LOG_ERROR"));
+
+        assertThat(e12.getProperty("log.level"), equalTo(3));
+        assertThat(e13.getProperty("log.level"), equalTo(1));
+        assertThat(e22.getProperty("log.level"), equalTo(3));
+        assertThat(e23.getProperty("log.level"), equalTo(1));
+        assertThat(e32.getProperty("log.level"), equalTo(3));
+        assertThat(e33.getProperty("log.level"), equalTo(1));
+
+        assertThat(((Exception)e13.getProperty("exception")).getMessage(), equalTo("exception 1"));
+        assertThat(((Exception)e23.getProperty("exception")).getMessage(), equalTo("exception 2"));
+        assertThat(((Exception)e33.getProperty("exception")).getMessage(), equalTo("exception 3"));
+    }
+
+    private Event ev(List<Event> events, String msg) {
+        return events.stream().filter(e -> e.getProperty("message").equals(msg)).findFirst().orElse(null);
+    }
+
+}
---
+++ pax-logging-it/src/test/java/org/ops4j/pax/logging/it/LogReaderIntegrationTest.java
@@ -0,0 +1,127 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.ops4j.pax.logging.it;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.logging.it.support.Helpers;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.log.LogEntry;
+import org.osgi.service.log.LogReaderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.ops4j.pax.exam.OptionUtils.combine;
+
+@RunWith(PaxExam.class)
+public class LogReaderIntegrationTest extends AbstractControlledIntegrationTestBase {
+
+    @Configuration
+    public Option[] configure() throws IOException {
+        return combine(
+                combine(baseConfigure(), defaultLoggingConfig()),
+
+                paxLoggingApi(),
+                paxLoggingLog4J1().noStart(),
+                paxLoggingLogback().noStart(),
+                paxLoggingLog4J2().noStart(),
+                configAdmin(),
+                eventAdmin()
+        );
+    }
+
+    @Test
+    public void logReaderServiceFromAll3Backends() throws Exception {
+        Logger logger = LoggerFactory.getLogger(LogReaderIntegrationTest.class);
+
+        ServiceReference<LogReaderService> sr = context.getServiceReference(LogReaderService.class);
+        assertNull(sr);
+
+        Bundle paxLoggingLog4J1 = Helpers.paxLoggingService(context);
+        Bundle paxLoggingLogback = Helpers.paxLoggingLogback(context);
+        Bundle paxLoggingLog4J2 = Helpers.paxLoggingLog4j2(context);
+
+        paxLoggingLog4J1.start();
+        sr = context.getServiceReference(LogReaderService.class);
+        assertNotNull(sr);
+        LogReaderService lrs = context.getService(sr);
+        CountDownLatch latch1 = new CountDownLatch(1);
+        final LogEntry[] pEntry = new LogEntry[1];
+        lrs.addLogListener(entry -> {
+            pEntry[0] = entry;
+            latch1.countDown();
+        });
+
+        logger.info("checking log4j1");
+        assertTrue(latch1.await(1, TimeUnit.SECONDS));
+        assertThat(pEntry[0].getMessage(), equalTo("checking log4j1"));
+
+        paxLoggingLog4J1.stop();
+        sr = context.getServiceReference(LogReaderService.class);
+        assertNull(sr);
+
+        paxLoggingLogback.start();
+        sr = context.getServiceReference(LogReaderService.class);
+        assertNotNull(sr);
+        lrs = context.getService(sr);
+        CountDownLatch latch2 = new CountDownLatch(1);
+        pEntry[0] = null;
+        lrs.addLogListener(entry -> {
+            pEntry[0] = entry;
+            latch2.countDown();
+        });
+
+        logger.info("checking logback");
+        assertTrue(latch2.await(1, TimeUnit.SECONDS));
+        assertThat(pEntry[0].getMessage(), equalTo("checking logback"));
+
+        paxLoggingLogback.stop();
+        sr = context.getServiceReference(LogReaderService.class);
+        assertNull(sr);
+
+        paxLoggingLog4J2.start();
+        sr = context.getServiceReference(LogReaderService.class);
+        assertNotNull(sr);
+        lrs = context.getService(sr);
+        CountDownLatch latch3 = new CountDownLatch(1);
+        pEntry[0] = null;
+        lrs.addLogListener(entry -> {
+            pEntry[0] = entry;
+            latch3.countDown();
+        });
+
+        logger.info("checking log4j2");
+        assertTrue(latch3.await(1, TimeUnit.SECONDS));
+        assertThat(pEntry[0].getMessage(), equalTo("checking log4j2"));
+    }
+
+}
