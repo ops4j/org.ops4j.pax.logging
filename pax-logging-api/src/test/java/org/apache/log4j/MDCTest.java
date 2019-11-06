14f42a0592ae514fde25af40b77a8f46019d55fe
Grzegorz Grzybek <gr.grzybek@gmail.com>
2019-11-06 13:13:34 +0100

    Allow switching MDC inheritance on/off. Fixes #46

--- pax-logging-api/src/test/java/org/apache/log4j/MDCTest.java
+++ pax-logging-api/src/test/java/org/apache/log4j/MDCTest.java
@@ -16,7 +16,15 @@
 package org.apache.log4j;
 
 import junit.framework.TestCase;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
+import org.ops4j.pax.logging.PaxContext;
+import org.ops4j.pax.logging.PaxLoggingConstants;
 
 public class MDCTest extends TestCase {
 
@@ -33,6 +41,63 @@ public class MDCTest extends TestCase {
 		Assert.assertEquals( "The return type is not an instance of Exception", Exception.class, mdcValue.getClass() );
 
 		Assert.assertSame( "The returned instance of Exception are not the same", value, mdcValue );
+	}
+	
+	public void testNonInheritedMDC() throws Exception {
+		System.setProperty(PaxLoggingConstants.LOGGING_CFG_INHERIT_THREAD_CONTEXT_MAP, "false");
+		
+		MDC.put("key", "value");
+		
+		final AtomicBoolean mdcIsSet = new AtomicBoolean(false);
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				mdcIsSet.set(MDC.get("key") != null);
+			}
+		});
+		t.start();
+		t.join();
+		
+		Assert.assertFalse( "The assigned key is not suppose to be null",  mdcIsSet.get());
+	}
+	
+	public void testInheritedMDC() throws Exception {
+		System.setProperty(PaxLoggingConstants.LOGGING_CFG_INHERIT_THREAD_CONTEXT_MAP, "true");
+		
+		MDC.put("key", "value");
+		
+		final AtomicBoolean mdcIsSet = new AtomicBoolean(false);
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				mdcIsSet.set(MDC.get("key") != null);
+			}
+		});
+		t.start();
+		t.join();
+		
+		Assert.assertTrue( "The assigned key is not suppose to be not null",  mdcIsSet.get());
+	}
+	
+	@Before
+	public void setup() throws Exception {
+		clear();
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		clear();
+	}
+	
+	private void clear() throws Exception {
+		System.getProperties().remove(PaxLoggingConstants.LOGGING_CFG_INHERIT_THREAD_CONTEXT_MAP);
+		
+		// Force the default context to be re-initialized
+		Field field = MDC.class.getDeclaredField("m_defaultContext");
+		field.setAccessible(true);
+		field.set(null, new PaxContext());
+		
+		MDC.clear();
 	}
 
 }
