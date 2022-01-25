4d1d526fc70db767e2a70290079c27c57769bf11
Grzegorz Grzybek <gr.grzybek@gmail.com>
2022-01-25 08:28:18 +0100

    [#456] Copy original classes from reload4j which are shaded in Pax Logging - from 1.2.18.3 (to change in next commit)

--- pax-logging-api/src/main/java/org/apache/log4j/MDC.java
+++ pax-logging-api/src/main/java/org/apache/log4j/MDC.java
@@ -18,122 +18,160 @@
 package org.apache.log4j;
 
 import java.util.Hashtable;
-import java.util.Map;
-
-import org.ops4j.pax.logging.PaxContext;
-import org.ops4j.pax.logging.PaxLoggingManager;
-import org.ops4j.pax.logging.PaxLoggingService;
+import org.apache.log4j.helpers.ThreadLocalMap;
 
 /**
-   The MDC class is similar to the {@link NDC} class except that it is
-   based on a map instead of a stack. It provides <em>mapped
-   diagnostic contexts</em>. A <em>Mapped Diagnostic Context</em>, or
-   MDC in short, is an instrument for distinguishing interleaved log
-   output from different sources. Log output is typically interleaved
-   when a server handles multiple clients near-simultaneously.
-
-   <p><b><em>The MDC is managed on a per thread basis</em></b>. A
-   child thread automatically inherits a <em>copy</em> of the mapped
-   diagnostic context of its parent.
-  
-   <p>The MDC class requires JDK 1.2 or above. Under JDK 1.1 the MDC
-   will always return empty values but otherwise will not affect or
-   harm your application.
-   
-   @since 1.2
-
-   @author Ceki G&uuml;lc&uuml; 
-*/
+ * The MDC class is similar to the {@link NDC} class except that it is based on
+ * a map instead of a stack. It provides <em>mapped diagnostic contexts</em>. A
+ * <em>Mapped Diagnostic Context</em>, or MDC in short, is an instrument for
+ * distinguishing interleaved log output from different sources. Log output is
+ * typically interleaved when a server handles multiple clients
+ * near-simultaneously.
+ * 
+ * <p>
+ * <b><em>The MDC is managed on a per thread basis</em></b>. A child thread
+ * automatically inherits a <em>copy</em> of the mapped diagnostic context of
+ * its parent.
+ * 
+ * <p>
+ * The MDC class requires JDK 1.2 or above. Under JDK 1.1 the MDC will always
+ * return empty values but otherwise will not affect or harm your application.
+ * 
+ * @since 1.2
+ * 
+ * @author Ceki G&uuml;lc&uuml;
+ */
 public class MDC {
-  
-  /** {@link PaxContext} used when {@link org.ops4j.pax.logging.PaxLoggingService} is not available */
-  private static PaxContext m_defaultContext = new PaxContext();
-  /** {@link PaxContext} obtained from {@link org.ops4j.pax.logging.PaxLoggingService} */
-  private static PaxContext m_context;
 
-  private
-  MDC() {
-  }
+    final static MDC mdc = new MDC();
 
-  /**
-   * <p>For all the methods that use the context, default, static, {@link PaxContext} may be used (tied to pax-logging-api
-   * bundle) if there's no available {@link PaxLoggingManager} or {@link PaxLoggingService}. If the service is
-   * available, it is <strong>always</strong> used to get service specific {@link PaxContext}.</p>
-   * <p>Refering <strong>always</strong> to {@link PaxLoggingService#getPaxContext()} is cheap operation, as it's
-   * only reference to fields.</p>
-   *
-   * <p>See: https://ops4j1.jira.com/browse/PAXLOGGING-247</p>
-   *
-   * @return m_context if the MDC should use the PaxContext object from the PaxLoggingManager,
-   *      or m_defaultContext if the logging manager is not set, or does not have its context available yet.
-   */
-  private static PaxContext getPaxContext() {
-    PaxLoggingManager manager = Logger.m_paxLogging;
-    if (manager != null) {
-      synchronized (MDC.class) {
-        PaxLoggingService service = manager.getPaxLoggingService();
-        m_context = service != null ? service.getPaxContext() : null;
-      }
+    static final int HT_SIZE = 7;
+
+    // boolean java1;
+
+    ThreadLocalMap tlm;
+
+    private MDC() {
+	tlm = new ThreadLocalMap();
     }
-    return m_context != null ? m_context : m_defaultContext;
-  }
 
-  /**
-     Put a context value (the <code>o</code> parameter) as identified
-     with the <code>key</code> parameter into the current thread's
-     context map.
+    /**
+     * Put a context value (the <code>o</code> parameter) as identified with the
+     * <code>key</code> parameter into the current thread's context map.
+     * 
+     * <p>
+     * If the current thread does not have a context map it is created as a side
+     * effect.
+     * 
+     */
+    static public void put(String key, Object o) {
+	if (mdc != null) {
+	    mdc.put0(key, o);
+	}
+    }
 
-     <p>If the current thread does not have a context map it is
-     created as a side effect.
-    
-   */
-  static
-  public
-  void put(String key, Object o) {
-    getPaxContext().put(key, o);
-  }
-  
-  /**
-     Get the context identified by the <code>key</code> parameter.
+    /**
+     * Get the context identified by the <code>key</code> parameter.
+     * 
+     * <p>
+     * This method has no side effects.
+     */
+    static public Object get(String key) {
+	if (mdc != null) {
+	    return mdc.get0(key);
+	}
+	return null;
+    }
 
-     <p>This method has no side effects.
-   */
-  static 
-  public
-  Object get(String key) {
-    return getPaxContext().get(key);
-  }
+    /**
+     * Remove the the context identified by the <code>key</code> parameter.
+     * 
+     */
+    static public void remove(String key) {
+	if (mdc != null) {
+	    mdc.remove0(key);
+	}
+    }
 
-  /**
-     Remove the the context identified by the <code>key</code>
-     parameter.
+    /**
+     * Get the current thread's MDC as a hashtable. This method is intended to be
+     * used internally.
+     */
+    public static Hashtable getContext() {
+	if (mdc != null) {
+	    return mdc.getContext0();
+	} else {
+	    return null;
+	}
+    }
 
-  */
-  static 
-  public
-  void remove(String key) {
-    getPaxContext().remove(key);
-  }
+    /**
+     * Remove all values from the MDC.
+     * 
+     * @since 1.2.16
+     */
+    public static void clear() {
+	if (mdc != null) {
+	    mdc.clear0();
+	}
+    }
 
+    private void put0(String key, Object o) {
+	if (tlm == null) {
+	    return;
+	} else {
+	    Hashtable ht = (Hashtable) ((ThreadLocalMap) tlm).get();
+	    if (ht == null) {
+		ht = new Hashtable(HT_SIZE);
+		((ThreadLocalMap) tlm).set(ht);
+	    }
+	    ht.put(key, o);
+	}
+    }
 
-  /**
-   * Get the current thread's MDC as a hashtable. This method is
-   * intended to be used internally.
-   *
-   * pax-logging note: this method has different return type than original {@code org.apache.log4j.MDC}.
-   * Originally this method returns {@link Hashtable}, but pax-logging's version
-   * returned {@link Map} for very long time.
-   * */
-  public static Map getContext() {
-    return getPaxContext().getContext();
-  }
+    private Object get0(String key) {
+	if (tlm == null) {
+	    return null;
+	} else {
+	    Hashtable ht = (Hashtable) ((ThreadLocalMap) tlm).get();
+	    if (ht != null && key != null) {
+		return ht.get(key);
+	    } else {
+		return null;
+	    }
+	}
+    }
 
-  /**
-   *  Remove all values from the MDC.
-   *  @since 1.2.16
-  */
-  public static void clear() {
-    getPaxContext().clear();
-  }
+    private void remove0(String key) {
+	if (tlm != null) {
+	    Hashtable ht = (Hashtable) ((ThreadLocalMap) tlm).get();
+	    if (ht != null) {
+		ht.remove(key);
+		// clean up if this was the last key
+		if (ht.isEmpty()) {
+		    clear0();
+		}
+	    }
+	}
+    }
+
+    private Hashtable getContext0() {
+	if (tlm == null) {
+	    return null;
+	} else {
+	    return (Hashtable) ((ThreadLocalMap) tlm).get();
+	}
+    }
+
+    private void clear0() {
+	if (tlm != null) {
+	    Hashtable ht = (Hashtable) ((ThreadLocalMap) tlm).get();
+	    if (ht != null) {
+		ht.clear();
+	    }
+
+	    tlm.remove();
+	}
+    }
 
 }
