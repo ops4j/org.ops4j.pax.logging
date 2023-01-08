a73507e18fc33a756d3f4b8aa7a1d28fe17108d1
Grzegorz Grzybek <gr.grzybek@gmail.com>
2023-01-08 08:38:36 +0100

    [#504] Take ch/qos/logback/classic/spi/PackagingDataCalculator unchanged from logback-classic-1.3.5

--- pax-logging-logback/src/main/java/ch/qos/logback/classic/spi/PackagingDataCalculator.java
+++ pax-logging-logback/src/main/java/ch/qos/logback/classic/spi/PackagingDataCalculator.java
@@ -15,9 +15,7 @@ package ch.qos.logback.classic.spi;
 
 import java.net.URL;
 import java.security.CodeSource;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 //import sun.reflect.Reflection;
 
@@ -25,18 +23,9 @@ import java.util.List;
 /**
  * Given a classname locate associated PackageInfo (jar name, version name).
  *
- * Original change was done in https://github.com/chrisdolan/logback/commits/eb1b03ac72f0534d9152279e39d85a81615a5e78
- * from logback-classic 0.9.30
- * Then I started over from the version from logback-classic 1.2.3 and did my best to reapply the OSGi fix
- *
  * @author James Strachan
- * @author Ceki G&uuml;lc&uuml;
- * @author Chris Dolan -- backport of http://jira.qos.ch/browse/LBCLASSIC-296 "Allow hook/override in PackagingDataCalculator"
- *
- * TODO - remove this class from pax-logging-logback when LBCLASSIC-296 is resolved and we pull in a Logback jar
- *     that has that fix
+ * @Ceki G&uuml;lc&uuml;
  */
-@SuppressWarnings("Duplicates")
 public class PackagingDataCalculator {
 
     final static StackTraceElementProxy[] STEP_ARRAY_TEMPLATE = new StackTraceElementProxy[0];
@@ -53,8 +42,8 @@ public class PackagingDataCalculator {
         // sun.reflect.Reflection class. However, this class will *not compile*
         // on JDKs lacking sun.reflect.Reflection.
         try {
-            //Reflection.getCallerClass(2);
-            //GET_CALLER_CLASS_METHOD_AVAILABLE = true;
+            // Reflection.getCallerClass(2);
+            // GET_CALLER_CLASS_METHOD_AVAILABLE = true;
         } catch (NoClassDefFoundError e) {
         } catch (NoSuchMethodError e) {
         } catch (UnsupportedOperationException e) {
@@ -62,19 +51,6 @@ public class PackagingDataCalculator {
             System.err.println("Unexpected exception");
             e.printStackTrace();
         }
-    }
-
-    private static final PackagingDataStrategy[] dataStrategies;
-
-    static {
-        List<PackagingDataStrategy> strategies = new ArrayList<>();
-        try {
-            strategies.add(new OsgiPackagingDataStrategy());
-        } catch (Throwable t) {
-            // ignore, probably org.osgi.framework.* is simply not in the classpath, or it's too old (need v4.2.0)
-        }
-        strategies.add(new DefaultPackagingDataStrategy());
-        dataStrategies = strategies.toArray(new PackagingDataStrategy[0]);
     }
 
     public void calculate(IThrowableProxy tp) {
@@ -90,13 +66,14 @@ public class PackagingDataCalculator {
         }
     }
 
+    @SuppressWarnings("unused")
     void populateFrames(StackTraceElementProxy[] stepArray) {
         // in the initial part of this method we populate package information for
         // common stack frames
         final Throwable t = new Throwable("local stack reference");
-        final StackTraceElement[] locateSTEArray = t.getStackTrace();
-        final int commonFrames = STEUtil.findNumberOfCommonFrames(locateSTEArray, stepArray);
-        final int localFirstCommon = locateSTEArray.length - commonFrames;
+        final StackTraceElement[] localSTEArray = t.getStackTrace();
+        final int commonFrames = STEUtil.findNumberOfCommonFrames(localSTEArray, stepArray);
+        final int localFirstCommon = localSTEArray.length - commonFrames;
         final int stepFirstCommon = stepArray.length - commonFrames;
 
         ClassLoader lastExactClassLoader = null;
@@ -106,7 +83,8 @@ public class PackagingDataCalculator {
         for (int i = 0; i < commonFrames; i++) {
             Class<?> callerClass = null;
             if (GET_CALLER_CLASS_METHOD_AVAILABLE) {
-                //callerClass = Reflection.getCallerClass(localFirstCommon + i - missfireCount + 1);
+                // callerClass = Reflection.getCallerClass(localFirstCommon + i - missfireCount
+                // + 1);
             }
             StackTraceElementProxy step = stepArray[stepFirstCommon + i];
             String stepClassname = step.ste.getClassName();
@@ -128,7 +106,8 @@ public class PackagingDataCalculator {
         populateUncommonFrames(commonFrames, stepArray, firsExactClassLoader);
     }
 
-    void populateUncommonFrames(int commonFrames, StackTraceElementProxy[] stepArray, ClassLoader firstExactClassLoader) {
+    void populateUncommonFrames(int commonFrames, StackTraceElementProxy[] stepArray,
+            ClassLoader firstExactClassLoader) {
         int uncommonFrames = stepArray.length - commonFrames;
         for (int i = 0; i < uncommonFrames; i++) {
             StackTraceElementProxy step = stepArray[i];
@@ -143,7 +122,11 @@ public class PackagingDataCalculator {
         if (cpd != null) {
             return cpd;
         }
-        return makePackagingFromType(type, className);
+        String version = getImplementationVersion(type);
+        String codeLocation = getCodeLocation(type);
+        cpd = new ClassPackagingData(codeLocation, version);
+        cache.put(className, cpd);
+        return cpd;
     }
 
     private ClassPackagingData computeBySTEP(StackTraceElementProxy step, ClassLoader lastExactClassLoader) {
@@ -153,25 +136,67 @@ public class PackagingDataCalculator {
             return cpd;
         }
         Class<?> type = bestEffortLoadClass(lastExactClassLoader, className);
-        return makePackagingFromType(type, className);
-    }
-
-    private ClassPackagingData makePackagingFromType(Class<?> type, String className) {
-        ClassPackagingData cpd = null;
-        if (type != null) {
-            for (PackagingDataStrategy strategy : dataStrategies) {
-                cpd = strategy.makePackagingFromType(type, className);
-                if (cpd != null) {
-                    break;
-                }
-            }
-        }
-        if (cpd == null) {
-            // should only happen if type == null
-            cpd = new ClassPackagingData("na", "na");
-        }
+        String version = getImplementationVersion(type);
+        String codeLocation = getCodeLocation(type);
+        cpd = new ClassPackagingData(codeLocation, version, false);
         cache.put(className, cpd);
         return cpd;
+    }
+
+    String getImplementationVersion(Class<?> type) {
+        if (type == null) {
+            return "na";
+        }
+        Package aPackage = type.getPackage();
+        if (aPackage != null) {
+            String v = aPackage.getImplementationVersion();
+            if (v == null) {
+                return "na";
+            } else {
+                return v;
+            }
+        }
+        return "na";
+
+    }
+
+    String getCodeLocation(Class<?> type) {
+        try {
+            if (type != null) {
+                // file:/C:/java/maven-2.0.8/repo/com/icegreen/greenmail/1.3/greenmail-1.3.jar
+                CodeSource codeSource = type.getProtectionDomain().getCodeSource();
+                if (codeSource != null) {
+                    URL resource = codeSource.getLocation();
+                    if (resource != null) {
+                        String locationStr = resource.toString();
+                        // now lets remove all but the file name
+                        String result = getCodeLocation(locationStr, '/');
+                        if (result != null) {
+                            return result;
+                        }
+                        return getCodeLocation(locationStr, '\\');
+                    }
+                }
+            }
+        } catch (Exception e) {
+            // ignore
+        }
+        return "na";
+    }
+
+    private String getCodeLocation(String locationStr, char separator) {
+        int idx = locationStr.lastIndexOf(separator);
+        if (isFolder(idx, locationStr)) {
+            idx = locationStr.lastIndexOf(separator, idx - 1);
+            return locationStr.substring(idx + 1);
+        } else if (idx > 0) {
+            return locationStr.substring(idx + 1);
+        }
+        return null;
+    }
+
+    private boolean isFolder(int idx, String text) {
+        return (idx != -1 && idx + 1 == text.length());
     }
 
     private Class<?> loadClass(ClassLoader cl, String className) {
@@ -217,92 +242,6 @@ public class PackagingDataCalculator {
             return null;
         } catch (Exception e) {
             e.printStackTrace(); // this is unexpected
-            return null;
-        }
-    }
-
-    public interface PackagingDataStrategy {
-        ClassPackagingData makePackagingFromType(Class<?> type, String className);
-    }
-
-    private static final class DefaultPackagingDataStrategy implements PackagingDataStrategy {
-        public ClassPackagingData makePackagingFromType(Class<?> type, String className) {
-            String version = getImplementationVersion(type);
-            String codeLocation = getCodeLocation(type);
-            return new ClassPackagingData(codeLocation, version);
-        }
-
-        private static String getImplementationVersion(Class<?> type) {
-            if (type == null) {
-                return "na";
-            }
-            Package aPackage = type.getPackage();
-            if (aPackage != null) {
-                String v = aPackage.getImplementationVersion();
-                if (v == null) {
-                    return "na";
-                } else {
-                    return v;
-                }
-            }
-            return "na";
-        }
-
-        private String getCodeLocation(Class<?> type) {
-            try {
-                if (type != null) {
-                    // file:/C:/java/maven-2.0.8/repo/com/icegreen/greenmail/1.3/greenmail-1.3.jar
-                    CodeSource codeSource = type.getProtectionDomain().getCodeSource();
-                    if (codeSource != null) {
-                        URL resource = codeSource.getLocation();
-                        if (resource != null) {
-                            String locationStr = resource.toString();
-                            // now lets remove all but the file name
-                            String result = getCodeLocation(locationStr, '/');
-                            if (result != null) {
-                                return result;
-                            }
-                            return getCodeLocation(locationStr, '\\');
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                // ignore
-            }
-            return "na";
-        }
-
-        private String getCodeLocation(String locationStr, char separator) {
-            int idx = locationStr.lastIndexOf(separator);
-            if (isFolder(idx, locationStr)) {
-                idx = locationStr.lastIndexOf(separator, idx - 1);
-                return locationStr.substring(idx + 1);
-            } else if (idx > 0) {
-                return locationStr.substring(idx + 1);
-            }
-            return null;
-        }
-
-        private boolean isFolder(int idx, String text) {
-            return (idx != -1 && idx + 1 == text.length());
-        }
-    }
-
-    private static final class OsgiPackagingDataStrategy implements PackagingDataStrategy {
-        public ClassPackagingData makePackagingFromType(Class<?> type, String className) {
-            try {
-                org.osgi.framework.Bundle bundle = org.osgi.framework.FrameworkUtil.getBundle(type);
-                if (bundle != null) {
-                    org.osgi.framework.Version bundleVersion = bundle.getVersion();
-                    String version = bundleVersion == org.osgi.framework.Version.emptyVersion ? "na" : bundleVersion.toString();
-                    String codeLocation = bundle.getSymbolicName();
-                    return new ClassPackagingData(codeLocation, version);
-                }
-            } catch (NoSuchMethodError e) {
-                // this means that FrameworkUtil is older than v4.2.0. Give up.
-            } catch (RuntimeException e) {    // at minimum: IllegalStateException, SecurityException
-                // go on to next strategy
-            }
             return null;
         }
     }
