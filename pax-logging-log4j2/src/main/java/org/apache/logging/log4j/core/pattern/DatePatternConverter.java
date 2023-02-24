0fbca990ff7ac663b59136791367223ad1918680
Grzegorz Grzybek <gr.grzybek@gmail.com>
2023-02-24 08:33:40 +0100

    [Fixes #513] Upgrade to Log4j2 2.20.0
    (cherry picked from commit 36dc75c2a0ef2db5aa9b54e27d0d48156ac640a7)

--- pax-logging-log4j2/src/main/java/org/apache/logging/log4j/core/pattern/DatePatternConverter.java
+++ pax-logging-log4j2/src/main/java/org/apache/logging/log4j/core/pattern/DatePatternConverter.java
@@ -26,9 +26,9 @@ import java.util.concurrent.atomic.AtomicReference;
 
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
-import org.apache.logging.log4j.core.util.Constants;
 import org.apache.logging.log4j.core.time.Instant;
 import org.apache.logging.log4j.core.time.MutableInstant;
+import org.apache.logging.log4j.core.util.Constants;
 import org.apache.logging.log4j.core.util.datetime.FastDateFormat;
 import org.apache.logging.log4j.core.util.datetime.FixedDateFormat;
 import org.apache.logging.log4j.core.util.datetime.FixedDateFormat.FixedFormat;
@@ -52,6 +52,10 @@ public final class DatePatternConverter extends LogEventPatternConverter impleme
 
         public String toPattern() {
             return null;
+        }
+
+        public TimeZone getTimeZone() {
+            return TimeZone.getDefault();
         }
     }
 
@@ -83,6 +87,11 @@ public final class DatePatternConverter extends LogEventPatternConverter impleme
         @Override
         public String toPattern() {
             return fastDateFormat.getPattern();
+        }
+
+        @Override
+        public TimeZone getTimeZone() {
+            return fastDateFormat.getTimeZone();
         }
     }
 
@@ -117,6 +126,11 @@ public final class DatePatternConverter extends LogEventPatternConverter impleme
         @Override
         public String toPattern() {
             return fixedDateFormat.getFormat();
+        }
+
+        @Override
+        public TimeZone getTimeZone() {
+            return fixedDateFormat.getTimeZone();
         }
     }
 
@@ -358,4 +372,12 @@ public final class DatePatternConverter extends LogEventPatternConverter impleme
         return formatter.toPattern();
     }
 
+    /**
+     * Gets the timezone used by this date format.
+     *
+     * @return the timezone used by this date format.
+     */
+    public TimeZone getTimeZone() {
+        return formatter.getTimeZone();
+    }
 }
