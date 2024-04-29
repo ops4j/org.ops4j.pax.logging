a85757bfd4152a8a4c0dfc50cf85d1fe30e28eed
Grzegorz Grzybek <gr.grzybek@gmail.com>
2024-04-29 10:47:25 +0200

    [Fixes #550][Closes #551] Synchronize shaded Log4j2 classes and Pax Logging alterations

---
+++ pax-logging-api/src/main/java/org/apache/logging/log4j/status/StatusConsoleListener.java
@@ -0,0 +1,214 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.logging.log4j.status;
+
+import static java.util.Objects.requireNonNull;
+
+import edu.umd.cs.findbugs.annotations.Nullable;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.message.Message;
+import org.ops4j.pax.logging.PaxLogger;
+import org.ops4j.pax.logging.spi.support.DefaultServiceLog;
+import org.ops4j.pax.logging.spi.support.FallbackLogFactory;
+import org.osgi.framework.FrameworkUtil;
+
+/**
+ * A {@link StatusListener} that writes to the console.
+ */
+@SuppressWarnings("UseOfSystemOutOrSystemErr")
+public class StatusConsoleListener implements StatusListener {
+
+    private final Lock lock = new ReentrantLock();
+
+    private final Level initialLevel;
+
+    private final PrintStream initialStream;
+
+    // `volatile` is necessary to correctly read the `level` without holding the lock
+    private volatile Level level;
+
+    // `volatile` is necessary to correctly read the `stream` without holding the lock
+    private volatile PrintStream stream;
+
+    private static PaxLogger logger = FallbackLogFactory.createFallbackLog(FrameworkUtil.getBundle(StatusLogger.class), "log4j2");
+
+    /**
+     * Constructs a {@link StatusConsoleListener} instance writing to {@link System#out} using the supplied level.
+     *
+     * @param level the level of status messages that should appear on the console
+     * @throws NullPointerException on null {@code level}
+     */
+    public StatusConsoleListener(final Level level) {
+        this(level, System.out);
+    }
+
+    /**
+     * Constructs a {@link StatusConsoleListener} instance using the supplied level and stream.
+     * <p>
+     * Make sure not to use a logger stream of some sort to avoid creating an infinite loop of indirection!
+     * </p>
+     *
+     * @param level the level of status messages that should appear on the console
+     * @param stream the stream to write to
+     * @throws NullPointerException on null {@code level} or {@code stream}
+     */
+    public StatusConsoleListener(final Level level, final PrintStream stream) {
+        this.initialLevel = this.level = requireNonNull(level, "level");
+        this.initialStream = this.stream = requireNonNull(stream, "stream");
+    }
+
+    /**
+     * Sets the level to a new value.
+     *
+     * @param level the new level
+     * @throws NullPointerException on null {@code level}
+     */
+    public void setLevel(final Level level) {
+        // Pax Logging: Configured directly through org.ops4j.pax.logging.spi.support.DefaultServiceLog.setLogLevel()
+    }
+
+    /**
+     * Sets the output stream to a new value.
+     *
+     * @param stream the new output stream
+     * @throws NullPointerException on null {@code stream}
+     * @since 2.23.0
+     */
+    public void setStream(final PrintStream stream) {
+        requireNonNull(stream, "stream");
+        // Check if a mutation (and locking!) is necessary at all
+        if (this.stream != stream) {
+            @Nullable OutputStream oldStream = null;
+            lock.lock();
+            try {
+                if (this.stream != stream) {
+                    oldStream = this.stream;
+                    this.stream = stream;
+                }
+            } finally {
+                lock.unlock();
+            }
+            if (oldStream != null) {
+                closeNonSystemStream(oldStream);
+            }
+        }
+    }
+
+    /**
+     * Returns the level for which the listener should receive events.
+     *
+     * @return the log level
+     */
+    @Override
+    public Level getStatusLevel() {
+        switch (logger.getLogLevel()) {
+            case AUDIT:
+                return Level.ALL;
+            case INFO:
+                return Level.INFO;
+            case WARN:
+                return Level.WARN;
+            case ERROR:
+                return Level.ERROR;
+            case TRACE:
+                return Level.TRACE;
+            case DEBUG:
+            default:
+                return Level.DEBUG;
+        }
+    }
+
+    /**
+     * Writes status messages to the console.
+     *
+     * @param data a status data
+     * @throws NullPointerException on null {@code data}
+     */
+    @Override
+    public void log(final StatusData data) {
+        requireNonNull(data, "data");
+        StackTraceElement ste = data.getStackTraceElement();
+        logMessage0(ste == null ? null : ste.getClassName(), data.getLevel(), data.getMarker(),
+                data.getMessage(), data.getThrowable());
+        final String formattedStatus = data.getFormattedStatus();
+        stream.println(formattedStatus);
+    }
+
+    private void logMessage0(String fqcn, Level level, Marker marker, Message msg, Throwable t) {
+        if (level == Level.ALL) {
+            // level treated as threshold...
+            logger.trace(msg.getFormattedMessage(), t, fqcn);
+        } else if (level == Level.FATAL) {
+            logger.fatal(msg.getFormattedMessage(), t, fqcn);
+        } else if (level == Level.ERROR) {
+            logger.error(msg.getFormattedMessage(), t, fqcn);
+        } else if (level == Level.WARN) {
+            logger.warn(msg.getFormattedMessage(), t, fqcn);
+        } else if (level == Level.INFO) {
+            logger.info(msg.getFormattedMessage(), t, fqcn);
+        } else if (level == Level.DEBUG) {
+            logger.debug(msg.getFormattedMessage(), t, fqcn);
+        } else if (level == Level.TRACE) {
+            logger.trace(msg.getFormattedMessage(), t, fqcn);
+        }
+    }
+
+    /**
+     * Adds package name filters to exclude.
+     *
+     * @param filters An array of package names to exclude.
+     * @deprecated This method is ineffective and only kept for binary backward compatibility.
+     */
+    @Deprecated
+    public void setFilters(final String... filters) {}
+
+    /**
+     * Resets the level and output stream to its initial values, and closes the output stream, if it is a non-system one.
+     */
+    @Override
+    public void close() {
+        final OutputStream oldStream;
+        lock.lock();
+        try {
+            oldStream = stream;
+            stream = initialStream;
+            level = initialLevel;
+        } finally {
+            lock.unlock();
+        }
+        closeNonSystemStream(oldStream);
+    }
+
+    private static void closeNonSystemStream(final OutputStream stream) {
+        // Close only non-system streams
+        if (stream != System.out && stream != System.err) {
+            try {
+                stream.close();
+            } catch (IOException error) {
+                // We are at the lowest level of the system.
+                // Hence, there is nothing better we can do but dumping the failure.
+                error.printStackTrace(System.err);
+            }
+        }
+    }
+}
---
+++ pax-logging-api/src/main/java/org/apache/logging/log4j/status/StatusData.java
@@ -0,0 +1,234 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.logging.log4j.status;
+
+import static java.util.Objects.requireNonNull;
+import static org.apache.logging.log4j.util.Chars.SPACE;
+
+import edu.umd.cs.findbugs.annotations.Nullable;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.Serializable;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.message.Message;
+
+/**
+ * The Status data.
+ */
+public class StatusData implements Serializable {
+
+    private static final long serialVersionUID = -4341916115118014017L;
+
+    private final Instant instant;
+
+    @Nullable
+    private final DateTimeFormatter instantFormatter;
+
+    @Nullable
+    private final StackTraceElement caller;
+
+    private final Level level;
+
+    private final Message message;
+
+    private final String threadName;
+
+    @Nullable
+    private final Throwable throwable;
+
+    // Pax Logging
+    private final Marker marker;
+
+    /**
+     * Constructs the instance using given properties.
+     *
+     * @param caller the method that created the event
+     * @param level a logging level
+     * @param message a message
+     * @param throwable the error occurred
+     * @param threadName the thread name
+     */
+    public StatusData(
+            @Nullable final StackTraceElement caller,
+            final Level level,
+            final Message message,
+            @Nullable final Throwable throwable,
+            @Nullable final String threadName) {
+        this(caller, level, message, throwable, threadName, null, Instant.now());
+    }
+
+    StatusData(
+            @Nullable final StackTraceElement caller,
+            final Level level,
+            final Message message,
+            @Nullable final Throwable throwable,
+            @Nullable final String threadName,
+            @Nullable final DateTimeFormatter instantFormatter,
+            final Instant instant) {
+        this.instantFormatter = instantFormatter;
+        this.instant = instant;
+        this.caller = caller;
+        this.level = requireNonNull(level, "level");
+        this.marker = null;
+        this.message = requireNonNull(message, "message");
+        this.throwable = throwable;
+        this.threadName =
+                threadName != null ? threadName : Thread.currentThread().getName();
+    }
+
+    StatusData(
+            @Nullable final StackTraceElement caller,
+            final Level level,
+            final Marker marker,
+            final Message message,
+            @Nullable final Throwable throwable,
+            @Nullable final String threadName,
+            @Nullable final DateTimeFormatter instantFormatter,
+            final Instant instant) {
+        this.instantFormatter = instantFormatter;
+        this.instant = instant;
+        this.caller = caller;
+        this.level = requireNonNull(level, "level");
+        this.marker = marker;
+        this.message = requireNonNull(message, "message");
+        this.throwable = throwable;
+        this.threadName =
+                threadName != null ? threadName : Thread.currentThread().getName();
+    }
+
+    /**
+     * Returns the instant of the event.
+     *
+     * @return the event's instant
+     */
+    public Instant getInstant() {
+        return instant;
+    }
+
+    /**
+     * Returns the instant of the event.
+     *
+     * @return the event's instant
+     * @deprecated Use {@link #getInstant()} instead.
+     */
+    @Deprecated
+    public long getTimestamp() {
+        return instant.toEpochMilli();
+    }
+
+    /**
+     * Returns the method that created the event.
+     *
+     * @return the method that created the event
+     */
+    @Nullable
+    public StackTraceElement getStackTraceElement() {
+        return caller;
+    }
+
+    /**
+     * Returns the logging level for the event.
+     *
+     * @return the event's logging level
+     */
+    public Level getLevel() {
+        return level;
+    }
+
+    // Pax Logging
+    public Marker getMarker() {
+        return marker;
+    }
+
+    /**
+     * Returns the message associated with the event.
+     *
+     * @return the message associated with the event
+     */
+    public Message getMessage() {
+        return message;
+    }
+
+    /**
+     * Returns the name of the thread associated with the event.
+     *
+     * @return the name of the thread associated with the event
+     */
+    public String getThreadName() {
+        return threadName;
+    }
+
+    /**
+     * Returns the error associated with the event.
+     *
+     * @return the error associated with the event
+     */
+    @Nullable
+    public Throwable getThrowable() {
+        return throwable;
+    }
+
+    /**
+     * Formats the event in to a log line for viewing.
+     *
+     * @return the formatted event
+     */
+    @SuppressFBWarnings(
+            value = "INFORMATION_EXPOSURE_THROUGH_AN_ERROR_MESSAGE",
+            justification = "Log4j prints stacktraces only to logs, which should be private.")
+    @SuppressWarnings("DefaultCharset")
+    public String getFormattedStatus() {
+        final StringBuilder sb = new StringBuilder();
+        final String formattedInstant =
+                instantFormatter != null ? instantFormatter.format(instant) : instant.toString();
+        sb.append(formattedInstant);
+        sb.append(SPACE);
+        sb.append(getThreadName());
+        sb.append(SPACE);
+        sb.append(level.toString());
+        sb.append(SPACE);
+        sb.append(message.getFormattedMessage());
+        final Object[] parameters = message.getParameters();
+        Throwable effectiveThrowable;
+        if (throwable == null && parameters != null && parameters[parameters.length - 1] instanceof Throwable) {
+            effectiveThrowable = (Throwable) parameters[parameters.length - 1];
+        } else {
+            effectiveThrowable = throwable;
+        }
+        if (effectiveThrowable != null) {
+            sb.append(SPACE);
+            final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            effectiveThrowable.printStackTrace(new PrintStream(baos));
+            /*
+             * https://errorprone.info/bugpattern/DefaultCharset
+             *
+             * Since Java 9 we'll be able to provide a charset.
+             */
+            sb.append(baos);
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return getMessage().getFormattedMessage();
+    }
+}
